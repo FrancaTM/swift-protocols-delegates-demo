12b7bbe7091eddb73147af885db28e9b6fca9865
FrancaTM <francatm@outlook.com>
2021-02-21 14:09:42 -0300

    just some protocol/delegate sample

--- ProtocolsDelegatesDemo/main.swift
+++ ProtocolsDelegatesDemo/main.swift
@@ -5,7 +5,61 @@
 //  Created by Tulio Marcos Franca on 21/02/21.
 //
 
-import Foundation
+protocol AdvancedLifeSupport {
+    func performCPR()
+}
 
-print("Hello, World!")
+class EmergencyCallHandler {
+    var delegate: AdvancedLifeSupport?
+    
+    func assessSituation() {
+        print("tell me what happened")
+    }
+    
+    func medicalEmergency() {
+        delegate?.performCPR()
+    }
+}
 
+struct Paramedic: AdvancedLifeSupport {
+    
+    init(handler: EmergencyCallHandler) {
+        handler.delegate = self
+    }
+    
+    func performCPR() {
+        print("paramedic does chest compressions, 30 per second")
+    }
+}
+
+class Doctor: AdvancedLifeSupport {
+    init(handler: EmergencyCallHandler) {
+        handler.delegate = self
+    }
+    
+    func performCPR() {
+        print("doctor does chest compressions, 30 per second")
+    }
+    
+    func useStethoscope() {
+        print("listening for heart sounds")
+    }
+}
+
+class Surgeon: Doctor {
+    override func performCPR() {
+        super.performCPR()
+        print("sings staying alive by BeeGees")
+    }
+    
+    func useElectricDrill() {
+        print("Whirrrr...")
+    }
+}
+
+let emilio = EmergencyCallHandler()
+//let pete = Paramedic(handler: emilio)
+let angela = Surgeon(handler: emilio)
+
+emilio.assessSituation()
+emilio.medicalEmergency()
